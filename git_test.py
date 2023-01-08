25628e58b66bc94a4290d38b9a723c61dd5900f9
Matvey_Skrpkin <Matvey.Skripkin@skoltech.ru>
2023-01-08 17:45:51 +0300

    Check

--- git_test.py
+++ git_test.py
@@ -1,22 +1,3 @@
-print('Second commit')
-print('Not to add')
-print('Check git add -p')
+print('This is fix2')
 
-print('Hmm')
-
-print('dsad')
-
-print('sadasd')
-
-print('sadasdffdgsg')
-
-
-print('Checjfdsf')
-
-print('Check 1 ')
-
-print('Some changes')
-
-print('New changes')
-
-print('Some new changes')
+print('Some feature')
